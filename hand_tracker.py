3dd74b9db5004bfa98e85efec882cd4d7524da12
Monica-Vajrapu <monicavajrapu@gmail.com>
2026-09-02 21:41:27 +0530

    Refactor mediapipe imports for consistency

--- hand_tracker.py
+++ hand_tracker.py
@@ -3,17 +3,6 @@ import mediapipe as mp
 from collections import deque
 import numpy as np
 
-# Robust import of mediapipe solutions for cross-platform and Python version compatibility
-try:
-    import mediapipe.python.solutions.hands as mp_hands
-    import mediapipe.python.solutions.drawing_utils as mp_draw
-except (ImportError, AttributeError):
-    try:
-        mp_hands = mp.solutions.hands
-        mp_draw = mp.solutions.drawing_utils
-    except AttributeError:
-        from mediapipe.solutions import hands as mp_hands, drawing_utils as mp_draw
-
 class HandTracker:
     def __init__(self, mode=False, max_hands=1, detection_con=0.7, track_con=0.5):
         self.mode = mode
@@ -21,14 +10,14 @@ class HandTracker:
         self.detection_con = detection_con
         self.track_con = track_con
 
-        self.mp_hands = mp_hands
+        self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(
             static_image_mode=self.mode,
             max_num_hands=self.max_hands,
             min_detection_confidence=self.detection_con,
             min_tracking_confidence=self.track_con
         )
-        self.mp_draw = mp_draw
+        self.mp_draw = mp.solutions.drawing_utils
         self.tip_ids = [4, 8, 12, 16, 20]
         
         # History for motion detection
